e7d4aa958a179374d31fc7e9189aed36996265ca
Jose Nicolielly <jnicolielly@gmail.com>
2018-04-24 11:23:49 -0400

    archivos modifcados por Jose Nicolielly para su revision

--- calculator.py
+++ calculator.py
@@ -1,20 +1,18 @@
 """
-Your module documentation here
+ Puedes realizar una clase en python llamada "calculator_class" que tenga un método llamado "sum" y que reciba como entrada una lista de números números y que de salida regrese la suma de los mismos. 
 """
-
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    Esta clase hereda de object y contiene el Metodo sum, el cual recibe el objeto self con el que el accede a los atributos y metodos de la clase CalculatorClass, este metodo sum tambien recibe como parametro de entrada una lista
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Declaro una variable sum que va a almacenar la suma de cada uno de los elementos que conforman la lista, al final retorna la suma de todos los numeros que conforman la lista
         """
-	sum = 0
-	for i in num_list:
-	    sum = sum + i
-	
+    	sum = 0
+    	for i in num_list:
+    	    sum = sum + i
 	return sum
 
--- primes.py
+++ primes.py
@@ -1,16 +1,21 @@
 """
-Your module documentation here
+Puedes realizar una clase en python llamada "prime_class" que tenga un método llamado "is_prime" y que reciba como entrada un número entero y que de salida regrese un True o False en boolean (no usar cadenas), para saber si sí es un número primo o no lo es, respectivamente. 
 """
-
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Esta clase contiene el Metodo is_prime, el cual recibe el objeto self con el que el accede a los atributos y metodos de la clase PrimeClass, este metodo is_prime tambien recibe como parametro de entrada un numero
     """
 
     def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+	"""declaro una variable para contar la cantidad de interacciones en que se calculara el resto de la division de acuerdo al numero ingresado, luego utilizo un for para calcular el resto de la division entre el numero, si es igual a cero va contando las veces que el resto de la division de ese numero es igual a cero, posteriormente valido si la cantidad de veces que de dividio es igual a dos entonces retorna True por que el numero es primo de lo contrario retorna False no es un numero primo"""
+     
+	    count = 0
+		for i in range(1,num_int+1):
+	    	if (num_int % i) == 0:
+	        	count = count + 1
+
+			if count == 2:
+	    		return True
+			else
+				return False
