4e4b519c069661b2631134b94b9ad73e6a8f178d
Jose Nicolielly <jnicolielly@gmail.com>
2018-04-24 19:39:35 -0400

    Actualizacion de los archivos .py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,23 @@
+# -*- coding: utf-8 -*-
 """
-Your module documentation here
+Puedes realizar una clase en python llamada calculator_class que
+tenga un método llamado sum y que reciba como entrada una lista de
+numeros numeros y que de salida regrese la suma de los mismos.
 """
-
-
 class CalculatorClass(object):
     """
-    Your class documentation here
+    Esta clase hereda de object y contiene el Metodo sum,
+    el cual recibe el objeto self con el que el accede a los atributos y
+    metodos de la clase CalculatorClass, este metodo sum tambien
+    recibe como parametro de entrada una lista
     """
-
     def sum(self, num_list):
         """
-        Your method documentation here
+        Declaro una variable sum que va a almacenar la suma de cada uno
+        de los elementos que conforman la lista, al final retorna la suma
+        de todos los numeros que conforman la lista
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        acum = 0
+        for number in num_list:
+            acum = acum + number
+        return acum
