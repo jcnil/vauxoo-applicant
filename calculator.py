7aa2a427c6d88af739b3ca83429672d9f419b476
Jose Nicolielly <jnicolielly@gmail.com>
2018-04-24 18:48:02 -0400

    actualizacion de los archivos calculator y prime

--- calculator.py
+++ calculator.py
@@ -1,18 +1,23 @@
+# -*- coding: utf-8 -*-
 """
- Puedes realizar una clase en python llamada "calculator_class" que tenga un método llamado "sum" y que reciba como entrada una lista de números números y que de salida regrese la suma de los mismos. 
+Puedes realizar una clase en python llamada calculator_class que
+tenga un método llamado sum y que reciba como entrada una lista de
+numeros numeros y que de salida regrese la suma de los mismos.
 """
-
 class CalculatorClass(object):
     """
-    Esta clase hereda de object y contiene el Metodo sum, el cual recibe el objeto self con el que el accede a los atributos y metodos de la clase CalculatorClass, este metodo sum tambien recibe como parametro de entrada una lista
+    Esta clase hereda de object y contiene el Metodo sum,
+    el cual recibe el objeto self con el que el accede a los atributos y
+    metodos de la clase CalculatorClass, este metodo sum tambien
+    recibe como parametro de entrada una lista
     """
-
     def sum(self, num_list):
         """
-        Declaro una variable sum que va a almacenar la suma de cada uno de los elementos que conforman la lista, al final retorna la suma de todos los numeros que conforman la lista
+        Declaro una variable sum que va a almacenar la suma de cada uno
+        de los elementos que conforman la lista, al final retorna la suma
+        de todos los numeros que conforman la lista
         """
-    	sum = 0
-    	for i in num_list:
-    	    sum = sum + i
-	return sum
-
+        sum = 0
+        for number in num_list:
+            sum = sum + number
+        return sum
--- primes.py
+++ primes.py
@@ -1,21 +1,33 @@
+# -*- coding: utf-8 -*-
 """
-Puedes realizar una clase en python llamada "prime_class" que tenga un método llamado "is_prime" y que reciba como entrada un número entero y que de salida regrese un True o False en boolean (no usar cadenas), para saber si sí es un número primo o no lo es, respectivamente. 
+Puedes realizar una clase en python llamada prime_class que tenga
+un método llamado is_prime y que reciba como entrada un número entero
+y que de salida regrese un True o False en boolean (no usar cadenas),
+para saber si sí es un número primo o no lo es, respectivamente.
 """
-
 class PrimeClass(object):
     """
-    Esta clase contiene el Metodo is_prime, el cual recibe el objeto self con el que el accede a los atributos y metodos de la clase PrimeClass, este metodo is_prime tambien recibe como parametro de entrada un numero
+    Esta clase contiene el Metodo is_prime, el cual recibe el objeto
+    self con el que el accede a los atributos y metodos de la clase PrimeClass,
+    este metodo is_prime tambien recibe como parametro de entrada un numero
     """
-
     def is_prime(self, num_int):
-	"""declaro una variable para contar la cantidad de interacciones en que se calculara el resto de la division de acuerdo al numero ingresado, luego utilizo un for para calcular el resto de la division entre el numero, si es igual a cero va contando las veces que el resto de la division de ese numero es igual a cero, posteriormente valido si la cantidad de veces que de dividio es igual a dos entonces retorna True por que el numero es primo de lo contrario retorna False no es un numero primo"""
-     
-	    count = 0
-		for i in range(1,num_int+1):
-	    	if (num_int % i) == 0:
-	        	count = count + 1
-
-			if count == 2:
-	    		return True
-			else
-				return False
+        """
+        declaro una variable para contar la cantidad de interacciones en que
+        se calculara el resto de la division de acuerdo al numero ingresado,
+        luego utilizo un for para calcular el resto
+        de la division entre el numero, si es igual a cero
+        va contando las veces que el resto de la division de ese
+        numero es igual a cero, posteriormente valido si
+        la cantidad de veces que de dividio es igual a dos
+        entonces retorna True es un numero primo de lo
+        contrario retorna False no es un numero primo
+        """
+        count = 0
+        for index in range(1, num_int+1):
+            if (num_int % index) == 0:
+                count = count + 1
+        if count == 2:
+            return True
+        else:
+            return False
